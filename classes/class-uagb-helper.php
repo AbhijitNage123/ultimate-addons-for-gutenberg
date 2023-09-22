49166c4de91c7239507769746576862ba40a848b
Abhijit Nage <abhijitn@bsf.io>
2023-09-22 10:46:01 +0530

    Created an common function to whiteList only allowed HTML tag

--- classes/class-uagb-helper.php
+++ classes/class-uagb-helper.php
@@ -1405,6 +1405,19 @@ if ( ! class_exists( 'UAGB_Helper' ) ) {
 			}
 			return $align_css;
 		}
+
+		/**
+		 * Get allowed HTML title tag.
+		 *
+		 * @param string $title_Tag
+		 * @param array $allowed_array
+		 * @param string $default_tag
+		 * @since x.x.x
+		 * @return string $allowed_title_tag.
+		 */
+		public static function title_tag_allowed_HTML( $title_Tag, $allowed_array, $default_tag ) {
+			return $allowed_title_tag = in_array( $title_Tag, $allowed_array, true ) ? $title_Tag : $default_tag;
+		}
 	}
 
 	/**
